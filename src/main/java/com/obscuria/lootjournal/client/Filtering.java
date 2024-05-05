1f1a0eda4cde78a96156640d711af89a71a2c64b
Obscuria <obscurialithium@gmail.com>
2024-05-05 18:21:51 -0300

    Update 3

--- src/main/java/com/obscuria/lootjournal/client/Filtering.java
+++ src/main/java/com/obscuria/lootjournal/client/Filtering.java
@@ -2,41 +2,69 @@ package com.obscuria.lootjournal.client;
 
 import com.obscuria.lootjournal.LootJournalConfig;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @OnlyIn(Dist.CLIENT)
-@SuppressWarnings("unused")
 public final class Filtering {
 
     public static boolean isAllowed(ItemStack stack) {
-        final var itemId = ForgeRegistries.ITEMS.getKey(stack.getItem());
-        if (itemsWhitelist().contains(itemId)) return true;
+        final var id = ForgeRegistries.ITEMS.getKey(stack.getItem());
+        if (itemsWhitelist().contains(id)) return true;
+        if (tabsWhitelist().contains(id)) return true;
         if (LootJournalConfig.blacklistedByDefault.get()) return false;
-        return !itemsBlacklist().contains(itemId);
-    }
-
-    private static List<ResourceLocation> tabsBlacklist() {
-        return remap(LootJournalConfig.tabsBlacklist.get());
-    }
-
-    private static List<ResourceLocation> itemsBlacklist() {
-        return remap(LootJournalConfig.itemsBlacklist.get());
-    }
-
-    private static List<ResourceLocation> tabsWhitelist() {
-        return remap(LootJournalConfig.tabsWhitelist.get());
+        if (itemsBlacklist().contains(id)) return false;
+        return !tabsBlacklist().contains(id);
     }
 
     private static List<ResourceLocation> itemsWhitelist() {
-        return remap(LootJournalConfig.itemsWhitelist.get());
+        return unwrapIds(LootJournalConfig.itemsWhitelist.get()).toList();
     }
 
-    private static List<ResourceLocation> remap(List<? extends String> list) {
-        return list.stream().map(ResourceLocation::new).toList();
+    private static List<ResourceLocation> itemsBlacklist() {
+        return unwrapIds(LootJournalConfig.itemsBlacklist.get()).toList();
+    }
+
+    private static List<ResourceLocation> tabsWhitelist() {
+        tryRebuildTabContents();
+        return unwrapIds(LootJournalConfig.tabsWhitelist.get())
+                .flatMap(Filtering::mapToTab)
+                .flatMap(Filtering::mapToContent)
+                .toList();
+    }
+
+    private static List<ResourceLocation> tabsBlacklist() {
+        tryRebuildTabContents();
+        return unwrapIds(LootJournalConfig.tabsBlacklist.get())
+                .flatMap(Filtering::mapToTab)
+                .flatMap(Filtering::mapToContent)
+                .toList();
+    }
+
+    private static void tryRebuildTabContents() {
+        if (CreativeModeTabs.CACHED_ENABLED_FEATURES == null)
+            CreativeModeTabs.tryRebuildTabContents(FeatureFlags.DEFAULT_FLAGS, false);
+    }
+
+    private static Stream<ResourceLocation> unwrapIds(List<? extends String> list) {
+        return list.stream().map(ResourceLocation::new);
+    }
+
+    private static Stream<CreativeModeTab> mapToTab(ResourceLocation id) {
+        final var result = CreativeModeTabRegistry.getTab(id);
+        return result == null ? Stream.empty() : Stream.of(result);
+    }
+
+    private static Stream<ResourceLocation> mapToContent(CreativeModeTab tab) {
+        return tab.getDisplayItems().stream().map(stack -> ForgeRegistries.ITEMS.getKey(stack.getItem()));
     }
 }
