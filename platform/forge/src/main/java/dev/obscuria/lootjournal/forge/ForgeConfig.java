0340b787ff74e499adb2bb306c99ffbafd0ef617
Obscuria <obscurialithium@gmail.com>
2025-09-14 06:52:36 -0300

    Update 5.0.0

--- platform/forge/src/main/java/dev/obscuria/lootjournal/forge/ForgeConfig.java
+++ platform/forge/src/main/java/dev/obscuria/lootjournal/forge/ForgeConfig.java
@@ -2,6 +2,7 @@ package dev.obscuria.lootjournal.forge;
 
 import dev.obscuria.lootjournal.ModConfig;
 import dev.obscuria.lootjournal.LootJournal;
+import dev.obscuria.lootjournal.ModConfigDefaults;
 import dev.obscuria.lootjournal.client.ItemPolicy;
 import dev.obscuria.lootjournal.client.render.Anchor;
 import dev.obscuria.lootjournal.client.render.PickupDrawStyle;
@@ -18,22 +19,26 @@ import java.util.List;
 public final class ForgeConfig
 {
     private static final ForgeConfigSpec SPEC;
-
     private static final ForgeConfigSpec.EnumValue<PickupDrawStyle> STYLE;
-    private static final ForgeConfigSpec.BooleanValue USE_RARITY_COLOR;
-    private static final ForgeConfigSpec.ConfigValue<String> ITEMS_COLOR;
-    private static final ForgeConfigSpec.ConfigValue<String> GROUPED_ITEMS_COLOR;
-    private static final ForgeConfigSpec.ConfigValue<String> EXPERIENCE_COLOR;
-    private static final ForgeConfigSpec.BooleanValue DISPLAY_EXPERIENCE;
-    private static final ForgeConfigSpec.BooleanValue DISPLAY_TOTAL;
-
+    private static final ForgeConfigSpec.BooleanValue ITEM_ENTRY_DISPLAY;
+    private static final ForgeConfigSpec.BooleanValue ITEM_ENTRY_DISPLAY_TOTAL_AMOUNT;
+    private static final ForgeConfigSpec.BooleanValue ITEM_ENTRY_USE_ITEM_FORMATTING;
+    private static final ForgeConfigSpec.ConfigValue<String> ITEM_ENTRY_COLOR;
+    private static final ForgeConfigSpec.BooleanValue ITEM_ENTRY_ITALIC;
+    private static final ForgeConfigSpec.BooleanValue AGGREGATED_ENTRY_DISPLAY;
+    private static final ForgeConfigSpec.ConfigValue<String> AGGREGATED_ENTRY_COLOR;
+    private static final ForgeConfigSpec.BooleanValue AGGREGATED_ENTRY_ITALIC;
+    private static final ForgeConfigSpec.BooleanValue EXPERIENCE_ENTRY_DISPLAY;
+    private static final ForgeConfigSpec.ConfigValue<String> EXPERIENCE_ENTRY_COLOR;
+    private static final ForgeConfigSpec.BooleanValue EXPERIENCE_ENTRY_ITALIC;
     private static final ForgeConfigSpec.EnumValue<Anchor> ANCHOR;
-    private static final ForgeConfigSpec.IntValue ANCHOR_OFFSET;
-    private static final ForgeConfigSpec.IntValue NOTIFICATION_SEPARATION;
-    private static final ForgeConfigSpec.IntValue NOTIFICATION_LIFETIME;
-    private static final ForgeConfigSpec.IntValue MAX_VISIBLE_NOTIFICATIONS;
-    private static final ForgeConfigSpec.IntValue MAX_QUEUED_NOTIFICATIONS;
-
+    private static final ForgeConfigSpec.IntValue ANCHOR_PIXEL_OFFSET;
+    private static final ForgeConfigSpec.IntValue ANCHOR_PERCENT_OFFSET;
+    private static final ForgeConfigSpec.IntValue SCALE;
+    private static final ForgeConfigSpec.IntValue SEPARATION;
+    private static final ForgeConfigSpec.IntValue LIFETIME;
+    private static final ForgeConfigSpec.IntValue DISPLAY_CAPACITY;
+    private static final ForgeConfigSpec.IntValue QUEUE_CAPACITY;
     private static final ForgeConfigSpec.EnumValue<ItemPolicy> DEFAULT_ITEM_POLICY;
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_ID_BLACKLIST;
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_ID_WHITELIST;
@@ -49,19 +54,32 @@ public final class ForgeConfig
     public static void onUpdate(final ModConfigEvent event)
     {
         LootJournal.CONFIG.style = STYLE.get();
-        LootJournal.CONFIG.useRarityColor = USE_RARITY_COLOR.get();
-        LootJournal.CONFIG.itemsColor = ModConfig.hexToInt(ITEMS_COLOR.get());
-        LootJournal.CONFIG.groupedItemsColor = ModConfig.hexToInt(GROUPED_ITEMS_COLOR.get());
-        LootJournal.CONFIG.experienceColor = ModConfig.hexToInt(EXPERIENCE_COLOR.get());
-        LootJournal.CONFIG.displayExperience = DISPLAY_EXPERIENCE.get();
-        LootJournal.CONFIG.displayTotal = DISPLAY_TOTAL.get();
+
+        LootJournal.CONFIG.itemEntryDisplay = ITEM_ENTRY_DISPLAY.get();
+        LootJournal.CONFIG.itemEntryDisplayTotalAmount = ITEM_ENTRY_DISPLAY_TOTAL_AMOUNT.get();
+        LootJournal.CONFIG.itemEntryUseItemFormatting = ITEM_ENTRY_USE_ITEM_FORMATTING.get();
+        LootJournal.CONFIG.itemEntryStyle = ModConfig.createStyle(
+                ITEM_ENTRY_COLOR.get(),
+                ITEM_ENTRY_ITALIC.get());
+
+        LootJournal.CONFIG.aggregatedEntryDisplay = AGGREGATED_ENTRY_DISPLAY.get();
+        LootJournal.CONFIG.aggregatedEntryStyle = ModConfig.createStyle(
+                AGGREGATED_ENTRY_COLOR.get(),
+                AGGREGATED_ENTRY_ITALIC.get());
+
+        LootJournal.CONFIG.experienceEntryDisplay = EXPERIENCE_ENTRY_DISPLAY.get();
+        LootJournal.CONFIG.experienceEntryStyle = ModConfig.createStyle(
+                EXPERIENCE_ENTRY_COLOR.get(),
+                EXPERIENCE_ENTRY_ITALIC.get());
 
         LootJournal.CONFIG.anchor = ANCHOR.get();
-        LootJournal.CONFIG.anchorPixelOffset = ANCHOR_OFFSET.get();
-        LootJournal.CONFIG.separation = NOTIFICATION_SEPARATION.get();
-        LootJournal.CONFIG.lifetime = NOTIFICATION_LIFETIME.get();
-        LootJournal.CONFIG.displayCapacity = MAX_VISIBLE_NOTIFICATIONS.get();
-        LootJournal.CONFIG.queueCapacity = MAX_QUEUED_NOTIFICATIONS.get();
+        LootJournal.CONFIG.anchorPixelOffset = ANCHOR_PIXEL_OFFSET.get();
+        LootJournal.CONFIG.anchorPercentOffset = ANCHOR_PERCENT_OFFSET.get();
+        LootJournal.CONFIG.scale = SCALE.get();
+        LootJournal.CONFIG.separation = SEPARATION.get();
+        LootJournal.CONFIG.lifetime = LIFETIME.get();
+        LootJournal.CONFIG.displayCapacity = DISPLAY_CAPACITY.get();
+        LootJournal.CONFIG.queueCapacity = QUEUE_CAPACITY.get();
 
         LootJournal.CONFIG.defaultItemPolicy = DEFAULT_ITEM_POLICY.get();
         LootJournal.CONFIG.itemIdBlacklist = ModConfig.mapItems(ITEM_ID_BLACKLIST.get());
@@ -74,31 +92,48 @@ public final class ForgeConfig
     {
         final var builder = new ForgeConfigSpec.Builder();
 
-        builder.push("Display");
-        STYLE = builder.defineEnum("style", ModConfig.STYLE);
-        USE_RARITY_COLOR = builder.define("useRarityColor", ModConfig.USE_RARITY_COLOR);
-        ITEMS_COLOR = builder.define("itemsColor", ModConfig.intToHex(ModConfig.ITEMS_COLOR));
-        GROUPED_ITEMS_COLOR = builder.define("groupedItemsColor", ModConfig.intToHex(ModConfig.GROUPED_ITEMS_COLOR));
-        EXPERIENCE_COLOR = builder.define("experienceColor", ModConfig.intToHex(ModConfig.EXPERIENCE_COLOR));
-        DISPLAY_EXPERIENCE = builder.define("displayExperience", ModConfig.DISPLAY_EXPERIENCE);
-        DISPLAY_TOTAL = builder.define("displayTotal", ModConfig.DISPLAY_TOTAL);
+        builder.push("Display Options");
+        STYLE = builder.defineEnum("Message Style", ModConfigDefaults.STYLE);
+
+        builder.push("Item Pickup");
+        ITEM_ENTRY_DISPLAY = builder.define("Show Item Pickups", ModConfigDefaults.ITEM_ENTRY_DISPLAY);
+        ITEM_ENTRY_DISPLAY_TOTAL_AMOUNT = builder.define("Show Total Item Count", ModConfigDefaults.ITEM_ENTRY_DISPLAY_TOTAL_AMOUNT);
+        ITEM_ENTRY_USE_ITEM_FORMATTING = builder.define("Use Minecraft Formatting", ModConfigDefaults.ITEM_ENTRY_USE_ITEM_FORMATTING);
+        ITEM_ENTRY_COLOR = builder.define("Text Color", ModConfig.intToHex(ModConfigDefaults.ITEM_ENTRY_COLOR));
+        ITEM_ENTRY_ITALIC = builder.define("Italic Text", ModConfigDefaults.ITEM_ENTRY_ITALIC);
         builder.pop();
 
-        builder.push("Layout");
-        ANCHOR = builder.defineEnum("anchor", ModConfig.ANCHOR);
-        ANCHOR_OFFSET = builder.defineInRange("anchorOffset", ModConfig.ANCHOR_OFFSET, 0, 256);
-        NOTIFICATION_SEPARATION = builder.defineInRange("notificationSeparation", ModConfig.NOTIFICATION_SEPARATION, 0, 16);
-        NOTIFICATION_LIFETIME = builder.defineInRange("notificationLifetime", ModConfig.NOTIFICATION_LIFETIME, 0, 32);
-        MAX_VISIBLE_NOTIFICATIONS = builder.defineInRange("maxVisibleNotifications", ModConfig.MAX_VISIBLE_NOTIFICATIONS, 1, 64);
-        MAX_QUEUED_NOTIFICATIONS = builder.defineInRange("maxQueuedNotifications", ModConfig.MAX_QUEUED_NOTIFICATIONS, 0, 256);
+        builder.push("Aggregated Pickup");
+        AGGREGATED_ENTRY_DISPLAY = builder.define("Show Aggregated Pickups", ModConfigDefaults.AGGREGATED_ENTRY_DISPLAY);
+        AGGREGATED_ENTRY_COLOR = builder.define("Text Color", ModConfig.intToHex(ModConfigDefaults.AGGREGATED_ENTRY_COLOR));
+        AGGREGATED_ENTRY_ITALIC = builder.define("Italic Text", ModConfigDefaults.AGGREGATED_ENTRY_ITALIC);
         builder.pop();
 
-        builder.push("Filter");
-        DEFAULT_ITEM_POLICY = builder.defineEnum("defaultItemPolicy", ModConfig.DEFAULT_ITEM_POLICY);
-        ITEM_ID_BLACKLIST = builder.defineList("itemIdBlacklist", Lists.newArrayList(), it -> it instanceof String);
-        ITEM_ID_WHITELIST = builder.defineList("itemIdWhitelist", Lists.newArrayList(), it -> it instanceof String);
-        MOD_ID_BLACKLIST = builder.defineList("modIdBlacklist", Lists.newArrayList(), it -> it instanceof String);
-        MOD_ID_WHITELIST = builder.defineList("modIdWhitelist", Lists.newArrayList(), it -> it instanceof String);
+        builder.push("Experience Pickup");
+        EXPERIENCE_ENTRY_DISPLAY = builder.define("Show Experience Pickups", ModConfigDefaults.EXPERIENCE_ENTRY_DISPLAY);
+        EXPERIENCE_ENTRY_COLOR = builder.define("Text Color", ModConfig.intToHex(ModConfigDefaults.EXPERIENCE_ENTRY_COLOR));
+        EXPERIENCE_ENTRY_ITALIC = builder.define("Italic Text", ModConfigDefaults.EXPERIENCE_ENTRY_ITALIC);
+        builder.pop();
+
+        builder.pop();
+
+        builder.push("Layout Options");
+        ANCHOR = builder.defineEnum("Screen Position", ModConfigDefaults.ANCHOR);
+        ANCHOR_PIXEL_OFFSET = builder.defineInRange("Offset", ModConfigDefaults.ANCHOR_PIXEL_OFFSET, 0, 200);
+        ANCHOR_PERCENT_OFFSET = builder.defineInRange("Relative Offset", ModConfigDefaults.ANCHOR_PERCENT_OFFSET, 0, 50);
+        SCALE = builder.defineInRange("Message Scale", ModConfigDefaults.SCALE, 50, 100);
+        SEPARATION = builder.defineInRange("Line Spacing", ModConfigDefaults.SEPARATION, 0, 16);
+        LIFETIME = builder.defineInRange("Message Duration", ModConfigDefaults.LIFETIME, 0, 32);
+        DISPLAY_CAPACITY = builder.defineInRange("Max Visible Messages", ModConfigDefaults.DISPLAY_CAPACITY, 1, 64);
+        QUEUE_CAPACITY = builder.defineInRange("Message Queue Size", ModConfigDefaults.QUEUE_CAPACITY, 0, 256);
+        builder.pop();
+
+        builder.push("Filter Options");
+        DEFAULT_ITEM_POLICY = builder.defineEnum("Default Filter Rule", ModConfigDefaults.DEFAULT_ITEM_POLICY);
+        ITEM_ID_BLACKLIST = builder.defineList("Item ID Blacklist", Lists.newArrayList(), it -> it instanceof String);
+        ITEM_ID_WHITELIST = builder.defineList("Item ID Whitelist", Lists.newArrayList(), it -> it instanceof String);
+        MOD_ID_BLACKLIST = builder.defineList("Mod ID Blacklist", Lists.newArrayList(), it -> it instanceof String);
+        MOD_ID_WHITELIST = builder.defineList("Mod ID Whitelist", Lists.newArrayList(), it -> it instanceof String);
         builder.pop();
 
         SPEC = builder.build();
