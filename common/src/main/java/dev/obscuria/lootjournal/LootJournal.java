f32fd217ef055a8c4ba60f61529094c4b44b9896
Obscuria <obscurialithium@gmail.com>
2025-09-14 22:45:39 -0300

    Patch 5.0.1

--- common/src/main/java/dev/obscuria/lootjournal/LootJournal.java
+++ common/src/main/java/dev/obscuria/lootjournal/LootJournal.java
@@ -2,11 +2,15 @@ package dev.obscuria.lootjournal;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class LootJournal
 {
     public static final String MODID = "loot_journal";
+    public static final String DISPLAY_NAME = "Loot Journal";
     public static final ModConfig CONFIG = new ModConfig();
+    public static final Logger LOGGER = LoggerFactory.getLogger(DISPLAY_NAME);
 
     public static ResourceLocation key(String path)
     {
